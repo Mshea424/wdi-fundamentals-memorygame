10bae1e1d82b5f044b7f67912b0ec35e4623d3b9
Michael Shea <Mike@Mikes-MBP.attlocal.net>
2020-03-14 10:28:08 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,15 @@
 console.log("Up and running!");
-let cardOne = "queen";
-let carTwo = "king";
-let cardThree = "king";
-let cardFour = "queen";
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length === 2) {
+	if (cardOne === cardTwo) {
+		alert("You found a Match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+};
